ce48ad81823a9358e618f858936b5e95fad0cbd9
Mohit KJadon <mohitkjadon@MohitKJadon.local>
2024-12-08 19:46:46 +0530

    Adding Chef Cookbook and Docker-compose

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,35 @@
 # Recipe:: default
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
+
+# default.rb
+
+# Install Docker
+package 'docker'
+
+# Enable and start Docker service
+
+package 'docker.io'
+
+# Starting the Docker service
+service 'docker' do
+  action [:enable, :start]
+end
+
+# Run the first container (fixed container in Challenge 1)
+docker_container 'app_container' do
+  repo 'krishz0303/docker-challenge-solved-mohitkjadon'
+  tag 'latest'
+  port '80:80'
+  action :run
+end
+
+# Running the second container (OS metrics scraping container)
+docker_container 'metrics_scraper' do
+  repo 'ubuntu'
+  tag 'latest'
+  command 'sh -c "apt-get update && apt-get install -y python3-pip && pip3 install psutil flask && python3 /metrics_scraper.py"'
+  port '8080:8080'  # Expose the metrics endpoint
+  action :run
+end
+
